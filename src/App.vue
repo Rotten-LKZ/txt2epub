66561ba203248b714713024937fca334260e5ecd
Rotten-LKZ <Rotten_LKZ@163.com>
2022-07-26 13:08:42 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div class="container">
+    <span>{{ t('action.selected') }}</span>
+    <el-button type="primary" plain>
+      {{ t('action.select_file_button') }}
+    </el-button>
+  </div>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+
+.container {
+  padding: 14px;
+}
+</style>
