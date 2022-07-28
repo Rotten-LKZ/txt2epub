745ab30f1c760c404f50f27b0aa35e48053d9774
Rotten-LKZ <Rotten_LKZ@163.com>
2022-07-28 17:23:58 +0800

    refector: router

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createI18n } from 'vue-i18n'
-import VueRouter from 'vue-router'
-import routes from './routes'
+import router from './router'
 import App from './App.vue'
 import zh from './locales/zh.json'
 import en from './locales/en.json'
@@ -9,11 +8,6 @@ import en from './locales/en.json'
 const i18n = createI18n({
   locale: navigator.language.includes('zh') ? 'zh' : 'en',
   messages: { zh, en },
-})
-
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
-  routes,
 })
 
 createApp(App)
